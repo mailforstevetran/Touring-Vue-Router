c9ec88dd0a1432096419e60977b101a5740bd8b5
mailforstevetran <mailforstevetran@gmail.com>
2021-07-31 20:40:40 +0900

    a

---
+++ src/views/event/Edit.vue
@@ -0,0 +1,10 @@
+<template>    
+    <p> Edit the Event here</p>
+</template>
+
+<script>
+
+export default {
+  props: ['event']
+}
+</script>
---
+++ src/views/event/Register.vue
@@ -0,0 +1,10 @@
+<template>    
+   <p> Register for the Event</p>
+</template>
+
+<script>
+
+export default {
+  props: ['event']
+}
+</script>
