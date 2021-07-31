7bd2b4cc535a6f0fdc16ea938dafad35797415da
mailforstevetran <mailforstevetran@gmail.com>
2021-08-01 02:19:35 +0900

    a

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import EventRegister from '../views/event/Register.vue'
 import EventEdit from '../views/event/Edit.vue'
 import About from '../views/About.vue'
 import NotFound from '../views/NotFound.vue'
+import NetworkError from '../views/NetworkError.vue'
 
 const routes = [
   {
@@ -59,7 +60,13 @@ const routes = [
     name: '404Resource',
     component: NotFound,
     props: true
-  } 
+  },
+  {
+    path: '/network-error',
+    name: 'NetworkError',
+    component: NetworkError,
+    props: true
+  }  
 ]
 
 const router = createRouter({
