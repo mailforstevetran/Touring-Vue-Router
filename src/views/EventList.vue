4565c9f973b442e67482bdab47e15374ee84b72f
mailforstevetran <mailforstevetran@gmail.com>
2021-07-31 17:57:33 +0900

    a

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,31 +2,59 @@
   <h1>Events for Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <div class="pagination">
+    <router-link
+      id="page-prev"
+      :to= "{name:'EventList', query:{page: page - 1}}"
+      rel="prev"
+      v-if="page != 1"
+    >&#60; Prev</router-link
+    >
+    <router-link
+    id="next-prev"
+      :to= "{name:'EventList', query:{page: page + 1}}"
+      rel="next"
+      v-if= "hasNextPage"      
+    >Next &#63;</router-link>
+    </div>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
+import {watchEffect} from 'vue'
 
 export default {
   name: 'EventList',
+  props:['page'],
   components: {
     EventCard
   },
   data() {
     return {
-      events: null
+      events: null,
+      totalEvents: 0
     }
   },
   created() {
-    EventService.getEvents()
+    watchEffect(() => {
+      this.events = null
+    EventService.getEvents(2, this.page)
       .then(response => {
         this.events = response.data
+        this.totalEvents = response.headers['x-total-count']
       })
       .catch(error => {
         console.log(error)
       })
+      })
+  },
+  computed: {
+    hasNextPage() {
+      var totalPages = Math.ceil(this.totalEvents/2)
+      return this.page < totalPages
+    }
   }
 }
 </script>
